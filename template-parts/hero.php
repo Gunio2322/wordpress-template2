27248821d9b11134ccab794ab3409e92b3496677
Mariusz G <gunio2322@gmail.com>
2021-12-27 18:41:22 +0100

    flexislider delet

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -14,7 +14,7 @@
 
 			   <div id="hero-slider" class="flexslider">
 
-				   <ul class="slides">
+				   <ul class="slidesx">
 
 					   <!-- Slide -->
 					   <li>
@@ -25,21 +25,7 @@
 							</div>						
 					   </li>
 
-					   <!-- Slide -->
-					   <li>						
-							<div class="flex-caption">
-								<h1 >Pozycjonowanie SEO i optymalizacja kodu dla poprawy widocznośći w wyszukiwarkach.</h1>	
-								<p><a class="button stroke smoothscroll" href="#portfolio">See Our Works</a></p>									   
-							</div>					
-					   </li>
-
-					   <!-- Slide -->
-					   <li>
-						   <div class="flex-caption">
-								<h1 >Take a look at some of <a class="smoothscroll" href="#portfolio" title="">our works</a> or <a class="smoothscroll" href="#contact" title="">contact us</a> to discuss how we can help you.</h1>
-								<p><a class="button stroke smoothscroll" href="#contact">Get In Touch</a></p>										   
-							</div>
-					   </li>					              
+					   					              
 
 				   </ul>
 
